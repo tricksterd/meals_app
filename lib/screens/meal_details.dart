aed3713640c7b5cf43c8cd0ba11b11336ec58aeb
tricksterd <kozavkolena@gmail.com>
2024-08-28 23:33:45 +0300

    added implicit animation to star icon

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -21,7 +21,18 @@ class MealDetailsScreen extends ConsumerWidget {
           title: Text(meal.title),
           actions: [
             IconButton(
-              icon: Icon(isFavorite ? Icons.star : Icons.star_border),
+              icon: AnimatedSwitcher(
+                  duration: const Duration(milliseconds: 300),
+                  transitionBuilder: (child, animation) {
+                    return RotationTransition(
+                        turns: Tween<double>(begin: 0.8, end: 1.0)
+                            .animate(animation),
+                        child: child);
+                  },
+                  child: Icon(
+                    isFavorite ? Icons.star : Icons.star_border,
+                    key: ValueKey(isFavorite),
+                  )),
               onPressed: () {
                 final wasAdded = ref
                     .read(favoriteMealsProvider.notifier)
